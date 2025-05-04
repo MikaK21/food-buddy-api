b3c3c2b27de899ed318412a18fe3a719c1224ba3
Mika Kessler <mika.kessler213@gmail.com>
2025-05-04 11:54:02 +0200

    chore: initialize Spring Boot project with initial setup

---
+++ src/main/java/com/foodbuddy/food_buddy_api/FoodBuddyApiApplication.java
@@ -0,0 +1,13 @@
+package com.foodbuddy.food_buddy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBuddyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBuddyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodbuddy/food_buddy_api/FoodBuddyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodbuddy.food_buddy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBuddyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
